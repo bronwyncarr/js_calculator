e3520fc9fa9dc1b9707cf56856d01242231828d9
bronwyncarr <bronwynacarr@gmail.com>
2020-11-30 20:48:01 +1100

    catch action that isn't between 1 and 4

--- index.js
+++ index.js
@@ -13,37 +13,38 @@ const menu = () => {
 
 const userInput = () => {
     const action = parseInt(readlineSync.question('>'));
-    console.log('What is the first number?')
-    const num1 = parseInt(readlineSync.question('>'));
-    console.log('What is the second number?')
-    const num2 = parseInt(readlineSync.question('>'));
-    return {
-        action: action,
-        num1: num1,
-        num2: num2
+    if (action < 1 || action > 4) {
+        console.log("Sorry please choose a number between 1 and 4");
+    }
+    else {
+        console.log('What is the first number?')
+        const num1 = parseInt(readlineSync.question('>'));
+        console.log('What is the second number?')
+        const num2 = parseInt(readlineSync.question('>'));
+        return {
+            action: action,
+            num1: num1,
+            num2: num2
+        }
     }
 }
 
 const userSelection = (action, num1, num2) => {
     switch(action) {
         case 1:
-            result = equation.add(num1, num2)
-            break;
+            return equation.add(num1, num2)
         case 2:
-            result = equation.subtract(num1, num2)
-            break;
+            return equation.subtract(num1, num2)
         case 3:
-            result = equation.multiply(num1, num2)
-            break;
+            return equation.multiply(num1, num2)
         case 4:
-            result = equation.divide(num1, num2)
-            break;
-        default:
-            console.log('Sorry wrong option chosen.')
+            return equation.divide(num1, num2)
     }
 }
 
 menu()
 const selection = userInput()
-userSelection(selection.action, selection.num1, selection.num2)
-console.log(`The result is ${result}.`)
+if (selection) {
+    result = userSelection(selection.action, selection.num1, selection.num2)
+    console.log(`The result is ${result}.`)
+}
